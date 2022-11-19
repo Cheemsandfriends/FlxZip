57f439df011257d4fe79fc6b34ea4c5a8d80b65d
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-11-19 18:31:23 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,7 +8,9 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		#if (flixel != "5.0.0")
+		#if (flixel >= "5.0.0")
+		addChild(new FlxGame(0, 0, PlayState.new));
+		#else
 		addChild(new FlxGame(0, 0, PlayState));
 		#end
 	}
